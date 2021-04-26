b23748199d137904f71837a08a813bd3de2f5191
Gatotz <gatot.prastyo@an.tv>
2021-04-26 22:17:47 +0700

    update nuxt-config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,18 @@
-module.exports = {
+export default {
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'universal',
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'server',
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */  
   /*
   ** Headers of the page
   */
